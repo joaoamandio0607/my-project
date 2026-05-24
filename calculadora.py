e7f3abba2e98fb2bf7b9fc0c44bc3aa33f7161ca
João Amândio <joao.amaral0607@gmail.com>
2026-05-24 15:11:13 -0300

    feature: Calculadora

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+operator = str(input('Enter the operator (1 For addition, 2 for subtraction, 3 for multiplication, 4 for division): '))
+
+if operator=='1':
+    print('Result: ', num1+num2)
+elif operator=='2':
+    print('Result: ', num1-num2)
+elif operator=='3':
+    print('Result: ', num1*num2)
+elif operator=='4':
+    print('Result: ', num1/num2)
+else:
+    print('Please insert a valid procedure.')
