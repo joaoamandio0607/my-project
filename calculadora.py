af98497acdcdbc904b909e27e63baace64aedd70
João Guilherme <otreek11@gmail.com>
2026-05-24 16:16:31 -0300

    feat: add exponentiation to calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,8 @@
 num1 = float(input('Enter the first number: '))
 num2 = float(input('Enter the second number: '))
-operator = str(input('Enter the operator (1 For addition, 2 for subtraction, 3 for multiplication, 4 for division): '))
+operator = str(
+    input('Enter the operator (1 For addition, 2 for subtraction, 3 for multiplication, 4 for division, 5 for exponentiation): ')
+)
 
 if operator=='1':
     print('Result: ', num1+num2)
@@ -10,5 +12,7 @@ elif operator=='3':
     print('Result: ', num1*num2)
 elif operator=='4':
     print('Result: ', num1/num2)
+elif operator=='5':
+    print('Result:', num1**num2)
 else:
     print('Please insert a valid procedure.')
